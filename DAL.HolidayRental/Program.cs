5317fed5d08490a2d904d81853d1fdfa729e7e32
Valerie Lametrie <v.lametrie@interface3.be>
2022-02-23 10:18:56 +0100

    first commit

---
+++ DAL.HolidayRental/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DAL.HolidayRental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
